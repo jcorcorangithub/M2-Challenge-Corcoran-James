d38d5fe62174176e29042398937e09a68f66291e
James Corcoran <jpcsoccer1234@gmail.com>
2022-02-22 10:16:02 -0500

    first commit of spring initializer file

---
+++ M2-Challenge/src/test/java/com/M2Challenge/M2Challenge/M2ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.M2Challenge.M2Challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
